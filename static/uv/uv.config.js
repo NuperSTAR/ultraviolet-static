432b5e8ee4e5cc32d6c87269f361d33a472e4820
Tamoghna Kommaraju <tamoghnak13@gmail.com>
2024-02-11 16:13:12 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://v4-omega-vert.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
